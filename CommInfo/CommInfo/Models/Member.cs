4d39a76e5d752f1b693ab580c84e96016d09b8a5
tokunomi <thewebupdater@gmail.com>
2016-02-25 07:11:03 -0800

    cs296n_Lab06-Win2016
    Lab 6: Adding Identity, Authorization and Authentication

---
+++ CommInfo/CommInfo/Models/Member.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CommInfo.Models
+{
+    public class Member
+    {
+        List<Message> message = new List<Message>();
+        public int MemberID { get; set; }
+        public int MessageID { get; set; }
+        [Required]
+        [StringLength(60, ErrorMessage = "The name you have entered is too long. Please enter a shorter name.")]
+        [Display(Name="First Name", Order=15000)]
+        public string NameFirst { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "Please enter a last name that is at least two characters long.")]
+        [Display(Name="Last Name", Order=15001)]
+        public string NameLast { get; set; }
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Please enter a username that is between 2 to 20 characters long.")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage="Please check your Email Address; it does not appear valid.")]
+        public string email { get; set; }
+    }
+}
---
+++ CommInfo/CommInfo/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommInfo.Startup))]
+namespace CommInfo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/auth/login")
+            });
+        }
+    } /* */
+
+   /* public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/auth/login")
+            });
+        }
+    }    */
+}
